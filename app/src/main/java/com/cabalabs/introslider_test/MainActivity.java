49ce7a898fc6a00a56d59db1cdc8005dc77a2add
chinmay <chinmay2997@gmail.com>
2019-02-07 23:11:07 +0530

    Chnages updated by dani

--- app/src/main/java/com/cabalabs/introslider_test/MainActivity.java
+++ app/src/main/java/com/cabalabs/introslider_test/MainActivity.java
@@ -1,13 +1,29 @@
 package com.cabalabs.introslider_test;
 
+import android.content.Context;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager mSlideViewPager;
+    private LinearLayout mDotLayout;
+
+    private SliderAdapter sliderAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSlideViewPager = (ViewPager) findViewById(R.id.slideViewPager);
+        mDotLayout = (LinearLayout) findViewById(R.id.dotsLayout);
+
+        sliderAdapter = new SliderAdapter(Context: this);
+
+        mSlideViewPager.setAdapter(sliderAdapter);
+
     }
 }
